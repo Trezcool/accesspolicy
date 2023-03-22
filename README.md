a703c5d064b1006102a72c3a6de9063cfe6ee4a0
Tresor Kambembo <tresor@propertyfinder.ae>
2023-03-22 08:19:44 +0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# access_policy
+# access-policy
+
+This initiative employs a declarative and explicit strategy for handling access control in Go projects. 
+It is consolidated in a single location and presented in a manner that is comprehensible to individuals with lesser technical expertise. 
+If you have experience with other declarative access frameworks, like AWS' IAM, you will find the syntax to be familiar.
+
+Example:
+
+```go
+package main
+
+func main() {
+	// Define a policy
+	policy := AccessPolicy{
+		Statements: []Statement{
+			{
+				Actions:   Actions{ActionAll},
+				Principal: PrincipalAuthenticated,
+				Conditions: Conditions{
+					func(user User, action Action) bool {
+						return user.GetID() == 1
+					},
+				},
+				Effect: EffectAllow,
+			},
+			{
+				Actions:   Actions{ActionAnySafe},
+				Principal: PrincipalAuthenticated,
+				Effect:    EffectAllow,
+			},
+		},
+	}
+
+	// Define a user and an action
+	usr := &user{id: 1}
+	action := HTTPMethodAction(http.MethodGet)
+
+	// Enforce the policy
+	if policy.HasPermission(usr, action) {
+		// Allow
+	} else {
+		// Deny
+	}
+}
+
+type user struct{ id uint }
+func (u *user) GetID() uint              { return u.id }
+func (u *user) GetGroups() []string      { return nil }
+func (u *user) GetPermissions() []string { return nil }
+func (u *user) IsAnonymous() bool        { return false }
+```
---
+++ access_policy.go
@@ -0,0 +1,224 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/samber/lo"
+)
+
+type User interface {
+	GetID() uint
+	IsAnonymous() bool
+	GetGroups() []string
+	GetPermissions() []string
+}
+
+type AccessPolicy struct {
+	Statements []Statement
+}
+
+type Statement struct {
+	Actions    Actions
+	Principal  Principal
+	Conditions Conditions
+	Effect     Effect
+}
+
+func (p *AccessPolicy) HasPermission(user User, action Action) bool {
+	if len(p.Statements) == 0 {
+		return false
+	}
+	return p.evaluateStatements(user, action)
+}
+
+func (p *AccessPolicy) evaluateStatements(user User, action Action) bool {
+	matched := p.getStatementsMatchingAction(action)
+	matched = p.getStatementsMatchingPrincipal(matched, user)
+	matched = p.getStatementsMatchingConditions(matched, user, action)
+
+	denied := p.getDeniedStatements(matched)
+
+	if len(matched) == 0 || len(denied) > 0 {
+		return false
+	}
+	return true
+}
+
+func (p *AccessPolicy) getStatementsMatchingAction(action Action) []Statement {
+	return lo.Filter(p.Statements, func(statement Statement, _ int) bool {
+		return statement.Actions.Match(action)
+	})
+}
+func (p *AccessPolicy) getStatementsMatchingPrincipal(statements []Statement, user User) []Statement {
+	return lo.Filter(statements, func(statement Statement, _ int) bool {
+		return statement.Principal.Match(user)
+	})
+}
+func (p *AccessPolicy) getStatementsMatchingConditions(statements []Statement, user User, action Action) []Statement {
+	return lo.Filter(statements, func(statement Statement, _ int) bool {
+		return statement.Conditions.Match(user, action)
+	})
+}
+func (p *AccessPolicy) getDeniedStatements(statements []Statement) []Statement {
+	return lo.Filter(statements, func(statement Statement, _ int) bool {
+		return statement.Effect != EffectAllow
+	})
+}
+
+type Action struct {
+	Name   string
+	IsSafe bool
+}
+
+type Actions []Action
+
+func (l Actions) Match(action Action) bool {
+	switch {
+	case lo.Contains(l, ActionAll):
+		return true
+	case lo.Contains(l, ActionAnySafe) && action.IsSafe:
+		return true
+	}
+	return lo.Contains(l, action)
+}
+
+var (
+	ActionAll     = Action{"*", false}
+	ActionAnySafe = Action{"any_safe", true}
+)
+
+var safeHTTPMethods = []string{
+	http.MethodGet,
+	http.MethodHead,
+	http.MethodOptions,
+}
+
+func HTTPMethodAction(method string) Action {
+	return Action{method, lo.Contains(safeHTTPMethods, method)}
+}
+
+type Principal string
+
+func (p Principal) Match(user User) bool {
+	switch {
+	case p == PrincipalAll:
+		return true
+	case p == PrincipalAuthenticated:
+		return !user.IsAnonymous()
+	case p == PrincipalAnonymous:
+		return user.IsAnonymous()
+	case strings.HasPrefix(string(p), principalGroupPrefix):
+		return p.matchGroups(user)
+	case strings.HasPrefix(string(p), principalPermissionPrefix):
+		return p.matchPermissions(user)
+	case strings.HasPrefix(string(p), principalUserPrefix):
+		return p.matchUsers(user)
+	default:
+		return false
+	}
+}
+
+func (p Principal) matchGroups(user User) bool {
+	pGroupsStr := strings.TrimPrefix(string(p), principalGroupPrefix)
+	pGroups := strings.Split(pGroupsStr, ",")
+	uGroups := user.GetGroups()
+	return len(lo.Intersect(pGroups, uGroups)) > 0
+}
+func (p Principal) matchPermissions(user User) bool {
+	pPermissionsStr := strings.TrimPrefix(string(p), principalPermissionPrefix)
+	pPermissions := strings.Split(pPermissionsStr, ",")
+	uPermissions := user.GetPermissions()
+	return len(lo.Intersect(pPermissions, uPermissions)) == len(pPermissions)
+}
+func (p Principal) matchUsers(user User) bool {
+	pUsersStr := strings.TrimPrefix(string(p), principalUserPrefix)
+	pUsers := strings.Split(pUsersStr, ",")
+	uID := strconv.Itoa(int(user.GetID()))
+	return lo.Contains(pUsers, uID)
+}
+
+const (
+	PrincipalAll           Principal = "*"
+	PrincipalAuthenticated           = "authenticated"
+	PrincipalAnonymous               = "anonymous"
+)
+
+const (
+	principalGroupPrefix      = "group:"
+	principalPermissionPrefix = "permission:"
+	principalUserPrefix       = "id:"
+)
+
+// GroupPrincipal will match any user that is in any of the groups
+func GroupPrincipal(group ...string) Principal {
+	return Principal(principalGroupPrefix + strings.Join(group, ","))
+}
+
+// PermissionPrincipal will match any user that has all the permissions TODO: support OR ?
+func PermissionPrincipal(permission ...string) Principal {
+	return Principal(principalPermissionPrefix + strings.Join(permission, ","))
+}
+
+// UserPrincipal will match any user whose ID is in the list
+func UserPrincipal(user ...string) Principal {
+	return Principal(principalUserPrefix + strings.Join(user, ","))
+}
+
+type Condition func(user User, action Action) bool
+
+type Conditions []Condition
+
+func (l Conditions) Match(user User, action Action) bool {
+	return lo.EveryBy(l, func(condition Condition) bool {
+		return condition(user, action)
+	})
+}
+
+type Effect string
+
+const (
+	EffectAllow Effect = "allow"
+	EffectDeny         = "deny"
+)
+
+func main() {
+	// Define a policy
+	policy := AccessPolicy{
+		Statements: []Statement{
+			{
+				Actions:   Actions{ActionAll},
+				Principal: PrincipalAuthenticated,
+				Conditions: Conditions{
+					func(user User, action Action) bool {
+						return user.GetID() == 1
+					},
+				},
+				Effect: EffectAllow,
+			},
+			{
+				Actions:   Actions{ActionAnySafe},
+				Principal: PrincipalAuthenticated,
+				Effect:    EffectAllow,
+			},
+		},
+	}
+
+	// Define a user and an action
+	usr := &user{id: 1}
+
+	// Enforce the policy
+	if policy.HasPermission(usr, HTTPMethodAction(http.MethodGet)) {
+		// Allow
+	} else {
+		// Deny
+	}
+}
+
+type user struct{ id uint }
+
+func (u *user) GetID() uint              { return u.id }
+func (u *user) GetGroups() []string      { return nil }
+func (u *user) GetPermissions() []string { return nil }
+func (u *user) IsAnonymous() bool        { return false }
